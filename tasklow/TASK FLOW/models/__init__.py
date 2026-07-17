2edd4733a267a01a6736bbc97afc3e71b316efff
Harmohini <harmohinichoudhary@gmail.com>
2026-07-17 09:42:06 +0530

    Delete tasklow directory

--- tasklow/TASK FLOW/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from models.user import User
-from models.project import Project
-from models.task import Task
-from models.workflow import Workflow
-from models.activity import Activity
-
-__all__ = ["User", "Project", "Task", "Workflow", "Activity"]
